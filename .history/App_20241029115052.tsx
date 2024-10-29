52ca403c093e6ffb8663bac8fef446f8bf120b33
Muhammad Amir <expressdev093@gmail.com>
2024-10-29 11:52:58 +0500

    first commit

--- .history/App_20241029115052.tsx
+++ .history/App_20241029115052.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export const App = () => {
   return (
     <SafeAreaView>
       <Text>App</Text>
-      <MaterialIcons name="home" size={28} />
     </SafeAreaView>
   );
 };
--- .history/App_20241029115136.tsx
+++ .history/App_20241029115136.tsx
@@ -6,7 +6,7 @@ export const App = () => {
   return (
     <SafeAreaView>
       <Text>App</Text>
-      <MaterialIcons name="home" size={28} />
+      <MaterialIcons name="home" />
     </SafeAreaView>
   );
 };
